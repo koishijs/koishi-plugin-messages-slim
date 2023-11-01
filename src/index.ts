c7a580354c5e7ad68558dc4d785d3b9252af9003
LittleC <26459759+XxLittleCxX@users.noreply.github.com>
2023-11-01 10:48:13 +0800

    feat: init

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,58 @@
 import { Context, Schema } from 'koishi'
+import { Message } from './message'
 
 export const name = 'messages-slim'
 
-export interface Config {}
+export interface Config { }
 
 export const Config: Schema<Config> = Schema.object({})
 
-export function apply(ctx: Context) {
-  // write your plugin here
+export const inject = ["database"]
+
+declare module 'koishi' {
+  interface Tables {
+    messages: Message
+  }
 }
+
+export function apply(ctx: Context) {
+  ctx.model.extend('messages', {
+    id: 'string',
+    content: 'text',
+    platform: 'string',
+    messageId: 'string',
+    userId: 'string',
+    timestamp: 'timestamp',
+    quoteId: 'string',
+    username: 'string',
+    nickname: 'string',
+    channelId: 'string',
+    lastUpdated: 'timestamp',
+    deleted: 'integer',
+    avatar: 'string',
+  })
+  ctx.on('message', async (session) => {
+    const msg = Message.fromSession(session)
+    await ctx.database.create('messages', msg)
+  })
+  ctx.on('message-deleted', async (session) => {
+    await ctx.database.set('messages', {
+      messageId: session.messageId,
+      platform: session.platform,
+    }, {
+      deleted: 1,
+      lastUpdated: new Date(),
+    })
+  })
+  ctx.on('message-updated', async (session) => {
+    await ctx.database.set('messages', {
+      messageId: session.messageId,
+      platform: session.platform,
+    }, {
+      content: session.content,
+      lastUpdated: new Date(),
+    })
+  })
+}
+
+export * from './message'
